9a41434510dae9684fba9d9cd3ee05c4a29fb726
Wesley <wesleyff100@gmail.com>
2024-01-03 20:53:28 -0300

    initial release

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 
+import { Button } from 'antd';
 import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
+import styled from 'styled-components';
 
 import reactLogo from './assets/react.svg';
 
@@ -10,25 +10,36 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <StyledLink isBlue={count > 4} className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </StyledLink>
   );
 }
+
+interface PStyledLink {
+  isBlue?: boolean;
+}
+
+const StyledLink = styled.div<PStyledLink>`
+  color: ${(props) => (props.isBlue ? 'blue' : 'pink')};
+  font-weight: bold;
+`;
 
 export default App;
